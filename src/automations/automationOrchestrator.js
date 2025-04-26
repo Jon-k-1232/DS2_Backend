863d48bc49b39551e6ec455195cf41b9b6444ca3
Jon Kimmel <Jonkimmel16@gmail.com>
2025-04-25 19:00:02 -0700

    activated tracker automation

--- src/automations/automationOrchestrator.js
+++ src/automations/automationOrchestrator.js
@@ -3,18 +3,20 @@ const runTimesheetAutomation = require('./automationScripts/timeTrackerAutomatio
 
 // Start automations. List all scheduled automations here.
 const scheduledAutomations = () => {
-   // Schedule the task to run at 03:29 PM every day
-   // schedule.scheduleJob('30 23 * * *', async () => {
-   //    console.log(`[${new Date().toISOString()}] Starting scheduled timesheets automation...`);
-   //    try {
-   //       const { successfulAccounts, failedAccounts } = await runTimesheetAutomation();
-   //       console.log(`[${new Date().toISOString()}] Scheduled timesheets automation completed.`);
-   //       console.log(`- Successful Accounts: ${successfulAccounts}`);
-   //       console.log(`- Failed Accounts: ${failedAccounts}`);
-   //    } catch (err) {
-   //       console.error(`[${new Date().toISOString()}] Unhandled error in scheduled automation: ${err.message}`);
-   //    }
-   // });
+   // Schedule the task to run at 10am, 12pm, 2pm, 4pm, 6pm, and 8pm everyday
+   schedule.scheduleJob('0 10,12,14,16,18,20 * * *', async () => runTimeTrackerAutomation());
 };
 
 module.exports = { scheduledAutomations };
+
+const runTimeTrackerAutomation = async () => {
+   console.log(`[${new Date().toISOString()}] Starting scheduled timesheets automation...`);
+   try {
+      const { successfulAccounts, failedAccounts } = await runTimesheetAutomation();
+      console.log(`[${new Date().toISOString()}] Scheduled timesheets automation completed.`);
+      console.log(`- Successful Accounts: ${successfulAccounts}`);
+      console.log(`- Failed Accounts: ${failedAccounts}`);
+   } catch (err) {
+      console.error(`[${new Date().toISOString()}] Unhandled error in scheduled automation: ${err.message}`);
+   }
+};
