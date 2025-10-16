f99f5b3c286a536881a6a12fd003a39e3e4687ee
Jon Kimmel <Jonkimmel16@gmail.com>
2025-10-16 16:42:58 -0700

    version bump

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,11 @@
-const { expect } = require('chai')
-const supertest = require('supertest')
+const { expect } = require('chai');
+const supertest = require('supertest');
 
-global.expect = expect
-global.supertest = supertest
+process.env.S3_BUCKET_NAME = process.env.S3_BUCKET_NAME || 'test-bucket';
+process.env.S3_REGION = process.env.S3_REGION || 'us-east-1';
+process.env.S3_ENDPOINT = process.env.S3_ENDPOINT || 'http://localhost';
+process.env.S3_ACCESS_KEY_ID = process.env.S3_ACCESS_KEY_ID || 'test-key';
+process.env.S3_SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY || 'test-secret';
+
+global.expect = expect;
+global.supertest = supertest;
